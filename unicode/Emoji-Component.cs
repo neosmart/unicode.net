c606df16f2c8696e1c527fb338f7ae716a4d04bb
Fabian Sauter <sauter.fabian@mailbox.org>
2019-03-25 20:16:40 +0100

    Added emoji groups

---
+++ unicode/Emoji-Component.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+
+namespace NeoSmart.Unicode
+{
+    // This file is machine-generated based on the official Unicode Consortium publication (https://unicode.org/Public/emoji/12.0/emoji-test.txt).
+    // See https://github.com/UWPX/Emoji-List-Parser for the generator.
+    public static partial class Emoji
+    {
+        /// <summary>
+        /// A (sorted) enumeration of all emoji in group: COMPONENT
+        /// Only contains fully-qualified and component emoji.
+        /// <summary>
+        public static IEnumerable<SingleEmoji> Component => new[] {
+			/* 🏻 */ LightSkinTone,
+			/* 🏼 */ MediumLightSkinTone,
+			/* 🏽 */ MediumSkinTone,
+			/* 🏾 */ MediumDarkSkinTone,
+			/* 🏿 */ DarkSkinTone,
+			/* 🦰 */ RedHair,
+			/* 🦱 */ CurlyHair,
+			/* 🦳 */ WhiteHair,
+			/* 🦲 */ Bald,
+        };
+    }
+}
