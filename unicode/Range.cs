038655074cbe803088e5be59a5d655152e181e75
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2021-10-09 15:41:44 -0500

    Add xmldocs to Range.cs

--- unicode/Range.cs
+++ unicode/Range.cs
@@ -3,17 +3,35 @@ using System.Collections.Generic;
 
 namespace NeoSmart.Unicode
 {
+    /// <summary>
+    /// A <c>Range</c> constitutes a range of <see>Codepoint</see> defined by the
+    /// <c>Begin</c> and <c>End</c> values, both of which are inclusive.
+    /// </summary>
     public class Range
     {
+        /// <summary>
+        /// The first codepoint in the range, inclusive.
+        /// </summary>
         public readonly Codepoint Begin;
+        /// <summary>
+        /// The last codepoint in the range, inclusive.
+        /// </summary>
         public readonly Codepoint End;
 
+        /// <summary>
+        /// Create a range constituting <c>Begin</c> and <c>End</c> codepoints. The two
+        /// values may be the same, but <paramref name="begin"/> must be less than or
+        /// equal to <paramref name="end"/>
+        /// </summary>
         public Range(Codepoint begin, Codepoint end)
         {
             Begin = begin;
             End = end;
         }
 
+        /// <summary>
+        /// Create a range constituting a single codepoint (<c>Begin == End</c>).
+        /// </summary>
         public Range(Codepoint value)
         {
             Begin = value;
@@ -27,6 +45,15 @@ namespace NeoSmart.Unicode
 
         static readonly string[] RangeSplit = new[] { "-", "–", "—", ".." };
         // Either a single hex codepoint or two separated by a hyphen
+
+        /// <summary>
+        /// Create a range from a string (hexadecimal) description of the range. A range
+        /// may be a single codepoint (in which case <c>Begin == End</c>) or a start and
+        /// end codepoint separated by a hyphen or two dots.
+        ///
+        /// Examples of valid ranges: <c>Range("0030..0039")</c>, <c>Range("0040")</c>,
+        /// and <c>Range("0600–06FF")</c>
+        /// </summary>
         public Range(string range)
         {
             // These are all different hyphens used on Wikipedia and in the UTR
